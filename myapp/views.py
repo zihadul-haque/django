398210b2b32fc33144e531c1a04012746c412084
Talukder Zihad <zihadkuet@yahoo.com>
2021-11-24 18:33:11 +0600

    form submitted and display

--- myapp/views.py
+++ myapp/views.py
@@ -15,4 +15,19 @@ def form(request):
     "test_form":new_form,
     "heading":"This from is created by django library"
     }
+
+    if request.method=='POST':
+        new_form=forms.user_forms(request.POST)
+
+        if new_form.is_valid():
+            user_name=new_form.cleaned_data['user_name']
+            user_dob=new_form.cleaned_data['user_dob']
+            user_email=new_form.cleaned_data['user_dob']
+
+            diction.update({"user_name":user_name})
+            diction.update({"user_dob":user_dob})
+            diction.update({"user_email":user_email})
+
+            diction.update({'form_submitted':"Yes"})
+
     return render(request,"myapp/form.html",context=diction)
